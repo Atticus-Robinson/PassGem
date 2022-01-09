c482d795a28acc414229307df86d332732e97b4e
Atticus Robinson <acerobinson2@gmail.com>
2022-01-09 14:10:14 -0600

    full working product

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,30 +1,42 @@
 // Assignment code here
-/*debugger;*/
+var leng;
+const upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+const lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+const numberCase = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+const specialCase = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '+', '=', '`', '~'];
 
-
+//ASk for length, parse answer, return value
 var lengthResponse = function() {
   leng = "";
 
+  //Validate entry, re-run if invalid
   while (leng === "" || leng === null) {
     leng = prompt("How many characters (Between 8-128");
+    if (leng < 8 || leng > 128) {
+      leng = "";
+    }
   }
 
+  //Parse and set leng value
   leng = parseInt(leng);
   return leng;
 }
 
 
+//Prompts storage object
 var prompts = {
   
-  passLength: lengthResponse(),
-
-  upper: window.confirm("Upper Case Characters?"),
-  lower: window.confirm("Lower Case Characters?"),
-  numeric: window.confirm("Numbers?"),
-  special: window.confirm("Special Characters?"),
-
+  upper: "",
+  lower: "",
+  numeric: "",
+  special: "",
 }
-
+var charPrompts = function() {
+  prompts.upper = window.confirm("Upper case letters?");
+  prompts.lower = window.confirm("Lower case letters?");
+  prompts.numeric = window.confirm("Numbers?");
+  prompts.special = window.confirm("Special characters?");
+}
 
 //Random 1-4
 function switchSelector() {
@@ -32,39 +44,102 @@ function switchSelector() {
   return switchValue;
 }
 
-let password = Array(leng);
-
-
 var randomFill = function() {
+  
+  //Set a = to random 1-4 with switchSelector()
   a = switchSelector();
+  console.log("Switch Selector: " + a);
+  
   switch(a) {
+    
+    //Upper Case
     case 1:
-      console.log("A");
-      break;
+      //If prompts.upper (from inital response) true
+      if (prompts.upper) {
+        //retVar equal to randomly selected value from upperCase array
+        retVar = randomCase(upperCase);
+        break;
+      }
+      //Else, re-run function
+      else {
+        randomFill();
+        break;
+      }
+    //Lower Case
     case 2:
-      console.log("B");
-      break;
+      if (prompts.lower) {
+        retVar = randomCase(lowerCase);
+        break;
+      } else {
+        randomFill();
+        break;
+      }
+    //Numbers
     case 3:
-      console.log("C");
-      break;
+      if (prompts.numeric) {
+        retVar = randomCase(numberCase);
+        break;
+      } else {
+        randomFill();
+        break;
+      }
+    //Special Characters
     case 4:
-      console.log("D");
-      break;
+      if (prompts.special) {
+        retVar = randomCase(specialCase);
+        break;
+      } else {
+        randomFill();
+        break;
+      }
     default:
       debugger;
+      randomFill();
       break;
   }
+  //Return selected array value to generatePassword()
+  return retVar;
 }
 
-console.log(switchSelector());
-console.log(switchSelector());
-console.log(switchSelector());
-console.log(randomFill());
+//Take array of either upper, lower, number, character
+var randomCase = function(caseArray) {
+  
+  //Choose random number based on length of array
+  b = Math.floor(Math.random() * caseArray.length);
+  console.log("Array number: " + b);
 
-/*for (let i = 0; i < password.length; i++) {
-  randomFill() = password[i];
+  //Set caseSelect equal to random number in array
+  caseSelect = caseArray[b];
+  console.log("Array choice: " + caseSelect);
 
-}*/
+  //Return chosen value to randomFill()
+  return caseSelect;
+}
+
+var generatePassword = function() {
+    //debugger;
+  //Reset array for additional actions
+  pwdArray = [];
+  i = 0;
+
+  lengthResponse();
+  charPrompts();
+  
+  //Iterate though array based on length, push from randomFill function
+  while (i < leng) {
+      var pushVar;
+      //Set pushVar = to return from randomFill() switch
+      pushVar = randomFill();
+      //Push var into array
+      pwdArray.push(pushVar);
+      //Add one to counter
+      i++;
+  }
+  //Condense array into single string, no quotes commas 
+  let finalString = pwdArray.join("");
+  return finalString;
+}
+
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
